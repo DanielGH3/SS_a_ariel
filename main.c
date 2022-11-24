335a83d0ab76c50c2788ce78dcfbf336a9f0bff2
DanielGH3 <118988238+DanielGH3@users.noreply.github.com>
2022-11-24 16:44:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,32 +11,35 @@ int main(int argc, char *argv[]){
         min = max;     
         max = temp;
     }
-
-    printf("The Armstrong numbers are:");
+    
+    FILE *f;
+    f = fopen (argv[3], "mode");
+    
+    fprintf(f, "The Armstrong numbers are:");
     for(int i = min; i <= max; i++){
         if(isArmstrong(i)){
-            printf(" %d", i);
+            fprintf(f, " %d", i);
         }
     }
 
-    printf("\nThe Palindromes are:");
+    fprintf(f, "\nThe Palindromes are:");
     for(int i = min; i <= max; i++){
         if(isPalindrome(i)){
-            printf(" %d", i);
+            fprintf(f, " %d", i);
         }
     }   
 
-    printf("\nThe Prime numbers are:");
+    fprintf(f, "\nThe Prime numbers are:");
     for(int i = min; i <= max; i++){
         if(isPrime(i)){
-            printf(" %d", i);
+            fprintf(f, " %d", i);
         }
     }    
 
-    printf("\nThe Strong numbers are:");
+    fprintf(f, "\nThe Strong numbers are:");
     for(int i = min; i <= max; i++){
         if(isStrong(i)){
-            printf(" %d", i);
+            fprintf(f, " %d", i);
         }
     }
 
